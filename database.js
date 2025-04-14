7f98d7795854f145fcfdab59a48dbaa1b860e71a
Solomon Aggrey <saggrey96@yahoo.com>
2025-04-14 13:17:49 +0000

    second commit

--- database.js
+++ database.js
@@ -1,17 +1,35 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
+const fs = require('fs');
+const { app } = require('electron');
 
-// Create or open the database
-const dbPath = path.join(__dirname, 'library.db');
+// Get the path to the user's app data folder
+const userDataPath = app.getPath('userData');
+const dbPath = path.join(userDataPath, 'library.db');
+
+// Path to the default database file packaged with the app
+const defaultDbPath = path.join(__dirname, 'library.db');
+
+// Copy the default DB to the user data folder if it doesn't exist
+if (!fs.existsSync(dbPath)) {
+    if (fs.existsSync(defaultDbPath)) {
+        fs.copyFileSync(defaultDbPath, dbPath);
+        console.log('Copied default database to user data folder.');
+    } else {
+        console.warn('Default database file not found. A new one will be created.');
+    }
+}
+
+// Open the database
 const db = new sqlite3.Database(dbPath, (err) => {
     if (err) {
         console.error('Error opening database:', err.message);
     } else {
-        console.log('Connected to the library database.');
+        console.log('Connected to the library database at:', dbPath);
     }
 });
 
-// Create the borrowed_books table
+// Create tables if they don't exist
 db.serialize(() => {
     db.run(`
         CREATE TABLE IF NOT EXISTS borrowed_books (
@@ -36,4 +54,5 @@ db.serialize(() => {
         )
     `);
 });
+
 module.exports = db;
